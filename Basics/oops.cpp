86f52675a18b68e586f8d6d80746153393cdfab2
{yashwanth} <{yashwanthburidi71@gmail.com}>
2026-06-23 15:56:30 +0530

    added new folder DSA and a file arrays.cpp

---
+++ Basics/oops.cpp
@@ -0,0 +1,21 @@
+// constructor method --having same name as class , 
+// no return type ,
+// as soon as object created constructor automatically called
+
+/*
+class Node{
+public:
+int data;
+Node *next;
+
+Node(int val){
+data=val;
+next=NULL;
+}
+
+}
+
+//Node *head=new Node(10)
+
+
+*/
---
+++ DSA/arrays.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[6]={1,2,3,4,5,6};
+    // for(int i=0; i<6; i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    for(int i:arr){
+        cout<<i<<" ";
+    }
+}
