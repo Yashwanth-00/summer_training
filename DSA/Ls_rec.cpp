ea5aa58181ee2a6d2a5894bac7a5fdaaaa20a8d4
{yashwanth} <{yashwanthburidi71@gmail.com}>
2026-06-30 16:09:08 +0530

    add new files

---
+++ DSA/Ls_rec.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+// int Ls(int arr[], int size, int key ){
+//     if(arr[0]==key) return size;
+// return Ls(arr+1, size-1, key);
+// }
+int Bs(int arr[], int e, int s, int key){
+    if()
+}
+int main(){
+int yrr[]={1,3,4,5,6,2};
+cout<<Bs(yrr,6,5);
+}
